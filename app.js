09542fe059a729dcc1ececcc73721c47bb53cca7
panda1 <blueblue0011@gmail.com>
2022-07-09 12:45:05 -0700

    fixed error

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ let company = {
     number_of_employees: 9849
 }
 
-if (company[yearly_revenue] >= 1000000) {
-    console.log(`name: ${company[names]}, ceo: ${company[ceo]}}, number of employees: ${company[number_of_employees]}`);
+if (company[`yearly_revenue`] >= 1000000) {
+    console.log(`name: ${company[`names`]}, ceo: ${company[`ceo`]}}, number of employees: ${company[`number_of_employees`]}`);
 }
