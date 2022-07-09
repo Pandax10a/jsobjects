a11313e05f1a00becd108e44e717cc42834b55ad
panda1 <blueblue0011@gmail.com>
2022-07-09 13:13:48 -0700

    added managers to company objects and while loop the names in it

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ let company = {
     names:`chicken and ribs`,
     yearly_revenue: 256890944,
     ceo: `ai`,
-    number_of_employees: 9849
+    number_of_employees: 9849,
+    managers: [`manager1`, `manager2`, `manager3`]
 }
 
 if (company[`yearly_revenue`] >= 1000000) {
-    console.log(`name: ${company[`names`]}, ceo: ${company[`ceo`]}}, number of employees: ${company[`number_of_employees`]}`);
+    console.log(`name: ${company[`names`]}, ceo: ${company[`ceo`]}, number of employees: ${company[`number_of_employees`]}`);
 } else {
     console.log(`try harder`);
+}
+
+let counter = 0;
+while (counter < company[`managers`].length) {
+    console.log(company[`managers`][counter]);
+    counter++;
 }
