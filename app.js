724cbe8032dc85abd7d153e703bcb7574659f150
panda1 <blueblue0011@gmail.com>
2022-07-09 12:41:16 -0700

    having trouble with one variable

---
+++ app.js
@@ -0,0 +1,10 @@
+let company = {
+    names:`chicken and ribs`,
+    yearly_revenue: 256890944,
+    ceo: `ai`,
+    number_of_employees: 9849
+}
+
+if (company[yearly_revenue] >= 1000000) {
+    console.log(`name: ${company[names]}, ceo: ${company[ceo]}}, number of employees: ${company[number_of_employees]}`);
+}
